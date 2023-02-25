809085d1ceb4af07e713788c3c1db60bbb4bc765
Frichox <fedequinterod30@gmail.com>
2023-02-24 21:37:56 -0300

    Proyecto Final Ticmas CV Quinteros Federico

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("botonmostrar").onclick = function () {
+    document.getElementById("estudiosincompletos").innerHTML = `
+    <p>2016-2018: Tecnicatura en turismo y transporte. Universidad Provincial de Córdoba</p>
+    <p>2019-2021: Tecnicatura en turismo y transporte. Instituto Superior Pascal</p>
+    `
+}
+
+document.getElementById("botonocultar").onclick = function () {
+    document.getElementById("estudiosincompletos").innerHTML = `
+    `
+}
